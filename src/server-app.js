e6a1dd4503578262a34e4eae96a9142a3245a513
Eric Lifka <eric.lifka@gmail.com>
2015-04-18 21:48:17 -0400

    setup sample http listener for server

---
+++ src/server-app.js
@@ -0,0 +1,5 @@
+require('babel/register');
+
+var httpServer = require('./server/http-server.js');
+
+httpServer.start();
---
+++ src/server/http-server.js
@@ -0,0 +1,10 @@
+import http from 'http';
+
+export var start = function () {
+    http.createServer((req, res) => {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World\n');
+    }).listen(1337, '127.0.0.1');
+
+    console.log('Server running at http://127.0.0.1:1337/');
+};
