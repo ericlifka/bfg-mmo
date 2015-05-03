cb7a26f685f41c9c98a72d64437ee5908b67208d
Eric Lifka <eric.lifka@gmail.com>
2015-05-03 15:14:45 -0400

    create a system for fixed chunks like spawn town

--- src/server/world-gen/chunk.js
+++ src/server/world-gen/chunk.js
@@ -4,23 +4,43 @@ const TILE_TYPES = [
     'B03', 'F22'
 ];
 
+const FIXED_CHUNKS = {
+    spawn: {
+        generate() {
+            return [];
+        }
+    }
+};
+
 export default class Chunk {
     constructor(id) {
         this.id = id;
-        let bounds = {
-            upperLeft: {
-                x: (32 * -50),
-                y: (32 * 50)
-            },
-            lowerRight: {
-                x: (32 * 50),
-                y: (32 * -50)
-            }
-        };
-        let generator = new ChunkGenerator(TILE_TYPES, bounds);
-        this.tiles = generator.generate();
         this.players = new Set();
         this.updates = [];
+
+        let generator;
+        if (FIXED_CHUNKS.hasOwnProperty(id)) {
+
+            generator = FIXED_CHUNKS[id];
+            this.tiles = generator.generate();
+
+        } else {
+
+            const bounds = {
+                upperLeft: {
+                    x: (32 * -50),
+                    y: (32 * 50)
+                },
+                lowerRight: {
+                    x: (32 * 50),
+                    y: (32 * -50)
+                }
+            };
+            generator = new ChunkGenerator(TILE_TYPES, bounds);
+
+        }
+
+        this.tiles = generator.generate();
     }
 
     serialize() {
