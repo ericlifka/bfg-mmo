1d49220b788eb3a0c6db27ee1c27e632b4b89752
Eric Lifka <eric.lifka@gmail.com>
2015-05-03 15:29:11 -0400

    spawn is now 4 blocks of water

--- src/server/world-gen/fixed-chunks/spawn.js
+++ src/server/world-gen/fixed-chunks/spawn.js
@@ -1,5 +1,10 @@
 export default {
     generate() {
-        return [];
+        return [
+            {position: {x: 0, y: 0}, type: 'F22'},
+            {position: {x: 32, y: 0}, type: 'F22'},
+            {position: {x: 0, y: 32}, type: 'F22'},
+            {position: {x: 32, y: 32}, type: 'F22'},
+        ];
     }
 };
