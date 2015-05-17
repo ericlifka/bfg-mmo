8471770702c03bc3ef9da86fefed963f034a5088
Eric Lifka <eric.lifka@gmail.com>
2015-05-17 11:32:59 -0400

    specking out the event structure blind

--- src/client/chat.js
+++ src/client/chat.js
@@ -1,6 +1,17 @@
 class Chat {
     constructor(connection) {
+        this.connection = connection;
 
+        this.createDocumentStructure();
+        this.connection.subscribe('chat-message', (...args) => this.newMessageEvent(...args));
+    }
+
+    createDocumentStructure() {
+
+    }
+
+    newMessageEvent() {
+        console.log(arguments);
     }
 }
 
