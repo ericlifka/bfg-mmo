a5bf549fa8cf49851eac9a269240c3877910057a
Eric Lifka <eric.lifka@gmail.com>
2015-05-17 11:58:45 -0400

    create dom structure

--- src/client/chat.js
+++ src/client/chat.js
@@ -7,7 +7,20 @@ class Chat {
     }
 
     createDocumentStructure() {
+        const chatFrame = document.createElement('div');
+        chatFrame.classList.add('chat-frame');
 
+        const messagesPane = document.createElement('div');
+        messagesPane.classList.add('messages-pane');
+
+        const messageInput = document.createElement('input');
+        messageInput.classList.add('message-input');
+        messageInput.setAttribute('type', 'text');
+        messageInput.setAttribute('placeholder', 'Send message');
+
+        chatFrame.appendChild(messagesPane);
+        chatFrame.appendChild(messageInput);
+        document.querySelector('body').appendChild(chatFrame);
     }
 
     newMessageEvent() {
